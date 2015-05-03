7893b49cac7aa9e56ff46cd2f428417709851411
Ricky Reusser <rsreusser@gmail.com>
2015-05-03 10:32:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# ndarray-gram-schmidt-qr
+
+[![Build Status](https://travis-ci.org/rreusser/ndarray-gram-schmidt-qr.svg?branch=master)](https://travis-ci.org/rreusser/ndarray-gram-schmidt-qr) [![npm version](https://badge.fury.io/js/ndarray-gram-schmidt-qr.svg)](http://badge.fury.io/js/ndarray-gram-schmidt-qr)
+
+A module for calculating the in-place [QR decomposition of a matrix](http://en.wikipedia.org/wiki/QR_decomposition)
+
+## Introduction
+
+The algorithm is the numerically stable variant of the Gram-Schmidt QR decomposition as found on p. 58 of Trefethen and Bau's [Numerical Linear Algebra](http://www.amazon.com/Numerical-Linear-Algebra-Lloyd-Trefethen/dp/0898713617). In pseudocode, the algorithm is:
+
+```
+for i = 1 to n
+  v_i = a_i
+
+for i = 1 to n
+  r_ii = ||v_i||
+  q_i = v_i / r_ii
+
+  for j = i+1 to n
+    r_ij = q_i' * v_j
+    v_j = v_j - r_ij * q_i
+```
+
+Currently only real number matrices are supported and only square matrices are tested.
+
+## Usage
+
+The algorithm currently only calculates the in-place QR decomposition and returns true on successful completion.
+
+```
+var qr = require('ndarray-gram-schmidt-qr'),
+    pool = require('ndarray-scratch');
+
+var A = ndarray( new Float64Array([1,2,7,4,5,1,7,4,9]), [3,3] );
+var R = pool.zeros( A.shape, A.dtype );
+
+qr( A, R );
+```
+
+Then the product A * R is approximately equal to the original matrix.
+
+## Credits
+(c) 2015 Ricky Reusser. MIT License
---
+++ test/test.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var qr = require('../gram-schmidt.js'),
+    assert = require('chai').assert,
+    ndarray = require('ndarray'),
+    pool = require('ndarray-scratch'),
+    gemm = require("ndgemm"),
+    ops = require('ndarray-complex'),
+    blas = require('ndarray-blas-level1-complex');
+
+
+describe("Gram-Schmidt QR", function() {
+
+  it('computes the in-place QR factorization of a square matrix',function() {
+    var i,j;
+    var n=3, m=3;
+    var A0_rdata = [1,  2,  3,4.5,  5,  6,  7,  8,  3];
+    var A0_idata = [2, -1,  2, -4,1.5,  0, -7,  2,  1];
+    var A_r = ndarray(new Float64Array(A0_rdata), [m,n]);
+    var A_i = ndarray(new Float64Array(A0_idata), [m,n]);
+    var A0_r = ndarray(new Float64Array(A0_rdata), [m,n]);
+    var A0_i = ndarray(new Float64Array(A0_idata), [m,n]);
+    var R_r = pool.zeros( A_r.shape, A_r.dtype );
+    var R_i = pool.zeros( A_r.shape, A_r.dtype );
+    var QR_r = pool.zeros( A_r.shape, A_r.dtype );
+    var QR_i = pool.zeros( A_r.shape, A_r.dtype );
+    var diff = pool.zeros( A_r.shape, A_r.dtype );
+
+    var success = qr(A_r, A_i, R_r, R_i);
+
+    assert(success);
+
+    // Confirm that all sub-diagonal entries are close to zero:
+    for(i=1; i<n; i++) {
+      for(j=0; j<i; j++) {
+        assert.closeTo( R_r.get(i,j), 0, 1e-8 );
+        assert.closeTo( R_i.get(i,j), 0, 1e-8 );
+      }
+    }
+
+    // Compute the matrix product and confirm it replicates the original:
+    for(i=0; i<m; i++) {
+      for(j=0; j<n; j++) {
+        var d = blas.dotu( A_r.pick(i,null), A_i.pick(i,null), R_r.pick(null,j), R_i.pick(null,j) );
+        assert.closeTo( d[0], A0_r.get(i,j), 1e-2 );
+        assert.closeTo( d[1], A0_i.get(i,j), 1e-2 );
+      }
+    }
+
+  });
+
+  it('returns false if the factorization fails',function() {
+    var i,j;
+    var n=2, m=2;
+    var A_r = ndarray(new Float64Array([1,2,2,4]), [m,n]);
+    var A_i = ndarray(new Float64Array([1,2,2,4]), [m,n]);
+    var R_r = pool.zeros( A_r.shape, A_r.dtype );
+    var R_i = pool.zeros( A_r.shape, A_r.dtype );
+
+    var success = qr(A_r, A_i, R_r, R_i);
+
+    assert(success === false);
+
+  });
+});
