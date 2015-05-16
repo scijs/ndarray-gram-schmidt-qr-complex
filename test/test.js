677cd49fa8518e5e9a573a84aab2e365bb3af8d0
Ricky Reusser <rsreusser@gmail.com>
2015-05-16 02:21:34 -0400

    Refactor it all

--- test/test.js
+++ test/test.js
@@ -1,63 +1,50 @@
-'use strict';
+'use strict'
 
-var qr = require('../gram-schmidt.js'),
-    assert = require('chai').assert,
+var qr = require('../'),
     ndarray = require('ndarray'),
     pool = require('ndarray-scratch'),
-    blas = require('ndarray-blas-level1-complex');
+    assert = require('assert'),
+    gemm = require('ndarray-blas-gemm-complex'),
+    ndt = require('ndarray-tests')
 
 
 describe("Gram-Schmidt QR", function() {
 
   it('computes the in-place QR factorization of a square matrix',function() {
-    var i,j;
-    var n=3, m=3;
-    var A0_rdata = [1,  2,  3,4.5,  5,  6,  7,  8,  3];
-    var A0_idata = [2, -1,  2, -4,1.5,  0, -7,  2,  1];
-    var A_r = ndarray(new Float64Array(A0_rdata), [m,n]);
-    var A_i = ndarray(new Float64Array(A0_idata), [m,n]);
-    var A0_r = ndarray(new Float64Array(A0_rdata), [m,n]);
-    var A0_i = ndarray(new Float64Array(A0_idata), [m,n]);
-    var R_r = pool.zeros( A_r.shape, A_r.dtype );
-    var R_i = pool.zeros( A_r.shape, A_r.dtype );
-    var QR_r = pool.zeros( A_r.shape, A_r.dtype );
-    var QR_i = pool.zeros( A_r.shape, A_r.dtype );
-    var diff = pool.zeros( A_r.shape, A_r.dtype );
+    var i,j
+    var n=3, m=3
+    var A0_r = ndarray([1, 2, 3,4.5, 5, 6, 7, 8, 3], [n,m])
+    var A0_i = ndarray([8,-2, 4, -2, 5, 3, 1, 2, 3], [n,m])
+    var A_r = ndarray([1, 2, 3,4.5, 5, 6, 7, 8, 3], [n,m])
+    var A_i = ndarray([8,-2, 4, -2, 5, 3, 1, 2, 3], [n,m])
+    var R_r = pool.zeros( A_r.shape )
+    var R_i = pool.zeros( A_r.shape )
 
-    var success = qr(A_r, A_i, R_r, R_i);
+    var QR_r = pool.zeros( [m,n] )
+    var QR_i = pool.zeros( [m,n] )
 
-    assert(success);
+    var success = qr(A_r, A_i, R_r, R_i)
 
-    // Confirm that all sub-diagonal entries are close to zero:
-    for(i=1; i<n; i++) {
-      for(j=0; j<i; j++) {
-        assert.closeTo( R_r.get(i,j), 0, 1e-8 );
-        assert.closeTo( R_i.get(i,j), 0, 1e-8 );
-      }
-    }
+    assert( success )
 
-    // Compute the matrix product and confirm it replicates the original:
-    for(i=0; i<m; i++) {
-      for(j=0; j<n; j++) {
-        var d = blas.dotu( A_r.pick(i,null), A_i.pick(i,null), R_r.pick(null,j), R_i.pick(null,j) );
-        assert.closeTo( d[0], A0_r.get(i,j), 1e-2 );
-        assert.closeTo( d[1], A0_i.get(i,j), 1e-2 );
-      }
-    }
+    gemm( A_r, A_i, R_r, R_i, QR_r, QR_i )
 
-  });
+    assert( ndt.approximatelyEqual( QR_r, A0_r, 1e-8 ) )
+    assert( ndt.approximatelyEqual( QR_i, A0_i, 1e-8 ) )
+
+  })
 
   it('returns false if the factorization fails',function() {
-    var i,j;
-    var n=2, m=2;
-    var A_r = ndarray(new Float64Array([1,2,2,4]), [m,n]);
-    var A_i = ndarray(new Float64Array([1,2,2,4]), [m,n]);
-    var R_r = pool.zeros( A_r.shape, A_r.dtype );
-    var R_i = pool.zeros( A_r.shape, A_r.dtype );
+    var i,j
+    var n=2, m=2
+    var A_r = ndarray(new Float64Array([1,2,2,4]), [n,m])
+    var A_i = ndarray(new Float64Array([0,0,0,0]), [n,m])
+    var R_r = pool.zeros( A_r.shape )
+    var R_i = pool.zeros( A_r.shape )
 
-    var success = qr(A_r, A_i, R_r, R_i);
+    var success = qr(A_r, A_i, R_r, R_i)
 
-    assert(success === false);
+    assert( ! success )
 
   });
 });
